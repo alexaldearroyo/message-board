3d1fe4551879ce9d071ec87f838c6357681766bd
alexaldearroyo <alexaldearroyo@gmail.com>
2024-01-18 05:38:13 +0900

    Express.js and fly.io configuration made.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = require('./routes/index');
 
 app.set('view engine', 'ejs');
@@ -13,6 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', router);
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`The application is running at http://localhost:${port}`);
 });
